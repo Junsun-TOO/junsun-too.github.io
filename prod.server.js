2de19aca55a6c1281f00658636805602e4e00c85
Junsun <599903219@qq.com>
2020-07-23 16:55:52 +0800

    prod5

--- prod.server.js
+++ prod.server.js
@@ -46,30 +46,10 @@ app.use('/api',apiRoutes);
 
 app.use(express.static('./dist'));
 
-
-module.exports = new Promise((resolve, reject) => {
-    portfinder.basePort = process.env.PORT || config.dev.port
-    portfinder.getPort((err, port) => {
-      if (err) {
-        reject(err)
-      } else {
-        // publish the new Port, necessary for e2e tests
-        process.env.PORT = port
-        // add port to devServer config
-        devWebpackConfig.devServer.port = port
-  
-        // Add FriendlyErrorsPlugin
-        devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-          compilationSuccessInfo: {
-            messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
-          },
-          onErrors: config.dev.notifyOnErrors
-          ? utils.createNotifierCallback()
-          : undefined
-        }))
-  
-        resolve(devWebpackConfig)
-      }
-    })
-  })
-  
+module.exports = app.listen(port, function (err) {
+	if (err) {
+		console.log(err);
+		return
+	}
+	console.log('Listening at http://localhost:' + port + '\n')
+});
