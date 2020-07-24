5dd2cba1d8754dc01f7580599f612669cb9953e0
Junsun <599903219@qq.com>
2020-07-24 19:03:02 +0800

    addto

--- prod.server.js
+++ prod.server.js
@@ -1,48 +1,48 @@
-var express=require('express');
-var config=require('./config/index.js');
+var express = require('express');
+var config = require('./config/index');
 
-var port=process.env.PORT || config.build.port;
+var port = process.env.PORT || config.build.port;
 
-var app=express();
+var app = express();
 
+var router = express.Router();
 
-var router=express.Router();
-
-router.get('/',function(req,res,next){
-	req.url='/index.html';
+router.get('/', function (req, res, next) {
+	req.url = '/index.html';
 	next();
 });
+
 app.use(router);
 
-// 获取数据
-var appData=require('./data.json');
-var seller=appData.seller;
-var goods=appData.goods;
-var ratings=appData.ratings;
+var appData = require('./data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
 
-//创建一个路由
-var apiRoutes=express.Router();
+var apiRoutes = express.Router();
 
-apiRoutes.get('/seller', function(req, res) {
-    res.json({
-      errno: 0,
-      data: seller
-    })
-});
-apiRoutes.get('/goods', function(req, res) {
-    res.json({
-      errno: 0,
-      data: goods
-    })
-});
-apiRoutes.get('/ratings', function(req, res) {
-    res.json({
-      errno: 0,
-      data: ratings
-    })
+apiRoutes.get('/seller', function (req, res) {
+	res.json({
+		errno: 0,
+		data: seller
+	});
 });
 
-app.use('/api',apiRoutes);
+apiRoutes.get('/goods', function (req, res) {
+	res.json({
+		errno: 0,
+		data: goods
+	});
+});
+
+apiRoutes.get('/ratings', function (req, res) {
+	res.json({
+		errno: 0,
+		data: ratings
+	});
+}); 
+
+app.use('/api', apiRoutes);
 
 app.use(express.static('./dist'));
 
